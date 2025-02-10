8e54f75a366e11aebde0f19c724c07cc0c8141b9
Julien-R-Besombes <julien.roland.besombes@gmail.com>
2025-02-10 21:57:45 +0100

    see points from pnas article

--- plot_viscosity_temperature_curve.py
+++ plot_viscosity_temperature_curve.py
@@ -11,11 +11,19 @@ data = np.genfromtxt(prefix+'viscosity_temperature_curve.csv', skip_header=1, de
 temperature = data['Temperature']
 viscosity = data['Viscosity']
 
+dataArt = np.genfromtxt('thPoints.csv', skip_header=1, delimiter=',', names=['Temperature', 'Viscosity'])
+
+# Assuming the CSV has columns 'Temperature' and 'Viscosity'
+temperatureArt = dataArt['Temperature']
+viscosityArt = dataArt['Viscosity']
+
 # Plot the data
 plt.figure(figsize=(10, 6))
-plt.plot(temperature, viscosity*1e3, marker='o', linestyle='-', color='b')
-plt.title('Viscosté en fonction de la température, jour 2')
+plt.plot(temperature, viscosity*1e3, label='Nos valeurs :(', marker='o', linestyle='-', color='b')
+plt.plot(temperatureArt, viscosityArt, label='Valeurs de l\'article :)', marker='o', linestyle='-', color='r')
+plt.title('Viscosité en fonction de la température, jour 2')
 plt.xlabel('Température (K)')
 plt.ylabel('Viscosité (mPa.s)')
 plt.grid(True)
+plt.legend()
 plt.show()
